e7117c9d7abfb935f9eb47c69458244a19ce672c
caok <caok1231@gmail.com>
2013-05-23 16:21:02 +0800

    add routes and views

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,25 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.post = function(req, res) {
+
+};
+
+exports.reg = function(req, res) {
+  res.render('reg', { title: '注册' });
+};
+
+exports.doReg = function(req, res) {
+};
+
+exports.login = function(req, res) {
+  res.render('login', { title: '登陆' });
+};
+
+exports.doLogin = function(req, res) {
+};
+
+exports.logout = function(req, res) {
+  res.render('/', { title: '已退出' });
+};
